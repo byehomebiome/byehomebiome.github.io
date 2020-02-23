939ad67ca56fbe83410fcf0bf3805e48d3c3b002
byehomebiome <60784936+byehomebiome@users.noreply.github.com>
2020-02-23 21:20:14 +0800

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -1,9 +1,17 @@
-function scrollPage() {
-  document.getElementById("leftdiv").scrollIntoView();
+function scrollPage() {   document.getElementById("leftdiv").scrollIntoView();
 }
 
 function expand(imgs) {
   var expandImg = document.getElementById("expandedImg");
   expandImg.src = imgs.src;
   expandImg.parentElement.style.display = "block";
+}
+
+function showNav() {
+  var navBar = document.getElementById("top-nav");
+  if (navBar.className === "topnav") {
+    navBar.className += " responsive";
+  } else {
+    navBar.className = "topnav";
+  }
 }
