a20bb301e19b7c653c4fbfdda6a5d52e883cd8af
byehomebiome <60784936+byehomebiome@users.noreply.github.com>
2020-03-21 15:12:44 +0800

    Update formjs.js

--- js/formjs.js
+++ js/formjs.js
@@ -53,8 +53,8 @@ function userrating() {
 					recordObject = JSON.parse(text);
 					
 					comments.innerHTML += "<h4> You rated Bye Home, Biomes a <span>" + recordObject.ratings + "</span> out of 5! </h4> We are delighted to know that you liked our website the most for its quality of being<span> " + recordObject.likesmost + 
-						"</span>. We shall also take into account the issues you have encountered ('" + recordObject.issue + "') and the suggestions you have offered ('" + recordObject.suggestion 
-						+ "') for Bye Home, Biomes. <br> The team would also like to express our gratitude for your pledge of donating " + recordObject.donates + " pesos. <br /><hr>";
+						"</span>. We shall also take into account the issues you have encountered ('<span>" + recordObject.issue + "</span>') and the suggestions you have offered ('<span>" + recordObject.suggestion 
+						+ "</span>') for Bye Home, Biomes. <br> The team would also like to express our gratitude for your pledge of donating <span>" + recordObject.donates + "</span> pesos. <br /><hr>";
 				}
 			}
 			else {comments.innerHTML = "Input your feedback."}	
